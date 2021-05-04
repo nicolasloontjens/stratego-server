31f62cd0e72768c177ca75a33c8e1c7fd3b496a3
Tobias <tobias.gabriels@student.howest.be>
2021-05-04 17:37:30 +0200

    comparator pawns

--- src/main/java/be/howest/ti/stratego2021/logic/ComparePawns.java
+++ src/main/java/be/howest/ti/stratego2021/logic/ComparePawns.java
@@ -2,13 +2,14 @@ package be.howest.ti.stratego2021.logic;
 
 import java.util.Comparator;
 
-public class ComparePawns implements Comparator<attackerRank, defenderRank>{
+public class ComparePawns implements Comparator<Integer>{
     public PawnTypes attackerRank;
     public PawnTypes defenderRank;
 
 
+
     @Override
-    public int compare(attackerRank o1, attackerRank o2) {
+    public int compare(Integer attackerRank, Integer defenderRank) {
         return 0;
     }
 }
