da84a8e62d5f699a5c47b6cb74c4e0d42e5eda11
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2021-05-10 11:29:36 +0200

    added tests for miner bomb and spy marshal

--- src/test/java/be/howest/ti/stratego2021/logic/PawnTest.java
+++ src/test/java/be/howest/ti/stratego2021/logic/PawnTest.java
@@ -7,8 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class PawnTest {
 
     @Test
-    void checkMinerBombLogic(){
-        Pawn miner = new Pawn()
+    void testMinerBombLogic(){
+        Pawn miner = new Pawn("blue","miner");
+        Pawn scout = new Pawn("blue","scout");
+        Pawn bomb = new Pawn("red","bomb");
+        assertTrue(scout.compareTo(bomb)<0);
+        assertEquals(1,miner.compareTo(bomb));
+    }
+    @Test
+    void testSpyMarshalLogic(){
+        Pawn spy = new Pawn("red","spy");
+        Pawn marshal = new Pawn("blue","marshal");
+        Pawn scout = new Pawn("blue","scout");
+        assertEquals(1,spy.compareTo(marshal));
+        assertTrue(spy.compareTo(scout)<0);
     }
 
 }
