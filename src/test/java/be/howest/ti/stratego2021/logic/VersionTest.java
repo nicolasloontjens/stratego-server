46f7f7674ff3fa5353b6fee46e586eb100583bbb
Martijn <martijn.maes@student.howest.be>
2021-05-12 10:09:55 +0200

    temp make test commit

--- src/test/java/be/howest/ti/stratego2021/logic/VersionTest.java
+++ src/test/java/be/howest/ti/stratego2021/logic/VersionTest.java
@@ -14,16 +14,73 @@ class VersionTest {
     List<List<String>> returnTinyConfig(){
         List<String> nullList = new ArrayList<>(Arrays.asList(null,null,null,null,null,null,null,null,null,null));
         List<String> flagList = new ArrayList<>(Arrays.asList("flag",null,null,null,null,null,null,null,null,null));
+        List<String> marshalList = new ArrayList<>(Arrays.asList(null,null,null,"marshal",null,null,null,null,null,null));
+        return new ArrayList<>(Arrays.asList(nullList,nullList,nullList,nullList,nullList,nullList,flagList,nullList,marshalList,nullList));
+    }
+
+    List<List<String>> returnFalseTinyConfig(){
+        List<String> nullList = new ArrayList<>(Arrays.asList(null,null,null,null,null,null,null,null,null,null));
+        List<String> flagList = new ArrayList<>(Arrays.asList("flag",null,null,null,null,null,null,null,null,null));
         List<String> marshalList = new ArrayList<>(Arrays.asList(null,"marshal",null,"marshal",null,null,null,null,null,null));
         return new ArrayList<>(Arrays.asList(nullList,nullList,nullList,nullList,nullList,nullList,flagList,nullList,marshalList,nullList));
+    }
+
+    List<List<String>> returnEmptyConfig(){
+        List<String> nullList = new ArrayList<>(Arrays.asList(null,null,null,null,null,null,null,null,null,null));
+        return new ArrayList<>(Arrays.asList(nullList,nullList,nullList,nullList,nullList,nullList,nullList,nullList,nullList,nullList));
+    }
+
+    List<List<String>> returnTooLargeConfig(){
+        List<String> nullList = new ArrayList<>(Arrays.asList(null,null,null,null,null,null,null,null,null,null));
+        List<String> flagList = new ArrayList<>(Arrays.asList("flag",null,null,null,null,null,null,null,null,null));
+        List<String> marshalList = new ArrayList<>(Arrays.asList(null,null,null,"marshal",null,null,null,null,null,null));
+        return new ArrayList<>(Arrays.asList(nullList,nullList,nullList,nullList,nullList,nullList,flagList,nullList,marshalList,nullList,nullList, nullList,nullList,nullList));
+    }
+
+    List<List<String>> returnTooSmallConfig(){
+        List<String> nullList = new ArrayList<>(Arrays.asList(null,null,null,null,null,null,null,null,null,null));
+        List<String> flagList = new ArrayList<>(Arrays.asList("flag",null,null,null,null,null,null,null,null,null));
+        List<String> marshalList = new ArrayList<>(Arrays.asList(null,null,null,"marshal",null,null,null,null,null,null));
+        return new ArrayList<>(Arrays.asList(flagList,nullList,marshalList));
     }
 
     @Test
     void checkIfVersionHasRightAmount(){
         GameManager gameManager = new GameManager();
         assertThrows(StrategoGameRuleException.class, ()-> {
-            gameManager.checkConfig("tiny", returnTinyConfig());
+            gameManager.checkConfig("tiny", returnFalseTinyConfig());
         });
     }
 
+    @Test
+    void checkIfRightVersion(){
+        GameManager gameManager = new GameManager();
+        assertThrows(StrategoGameRuleException.class, ()-> {
+            gameManager.checkConfig("mini", returnTinyConfig());
+        });
+    }
+
+    @Test
+    void checkIfLessAmount(){
+        GameManager gameManager = new GameManager();
+        assertThrows(StrategoGameRuleException.class, ()-> {
+            gameManager.checkConfig("tiny", returnEmptyConfig());
+        });
+    }
+
+    @Test
+    void checkIfConfigTooLarge(){
+        GameManager gameManager = new GameManager();
+        assertThrows(StrategoGameRuleException.class, ()-> {
+            gameManager.checkConfig("tiny", returnTooLargeConfig());
+        });
+    }
+
+    @Test
+    void checkIfConfigTooSmall(){
+        GameManager gameManager = new GameManager();
+        assertThrows(StrategoGameRuleException.class, ()-> {
+            gameManager.checkConfig("tiny", returnTooLargeConfig());
+        });
+    }
 }
