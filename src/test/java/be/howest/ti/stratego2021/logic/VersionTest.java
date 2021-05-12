5e3c72f9e5268363727bb6f66efd3d057dd069bd
Martijn <martijn.maes@student.howest.be>
2021-05-12 09:59:16 +0200

    first test

--- src/test/java/be/howest/ti/stratego2021/logic/VersionTest.java
+++ src/test/java/be/howest/ti/stratego2021/logic/VersionTest.java
@@ -1,19 +1,29 @@
 package be.howest.ti.stratego2021.logic;
 
+import be.howest.ti.stratego2021.logic.exceptions.StrategoGameRuleException;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VersionTest {
 
-/**    @Test
-    void checkIfVersionHasRightAmount(){
-        Version original = new Version("original",PieceCount.valueOf("ORIGINAL"));
-        assertEquals(1,original.getPieceCount().getGeneral());
-        assertEquals(8,original.getPieceCount().getScout());
-        Version tiny = new Version("original", PieceCount.valueOf("TINY"));
-        assertEquals(1,tiny.getPieceCount().getMarshal());
-        assertEquals(0,tiny.getPieceCount().getInfiltrator());
+    List<List<String>> returnTinyConfig(){
+        List<String> nullList = new ArrayList<>(Arrays.asList(null,null,null,null,null,null,null,null,null,null));
+        List<String> flagList = new ArrayList<>(Arrays.asList("flag",null,null,null,null,null,null,null,null,null));
+        List<String> marshalList = new ArrayList<>(Arrays.asList(null,"marshal",null,"marshal",null,null,null,null,null,null));
+        return new ArrayList<>(Arrays.asList(nullList,nullList,nullList,nullList,nullList,nullList,flagList,nullList,marshalList,nullList));
     }
-*/
+
+    @Test
+    void checkIfVersionHasRightAmount(){
+        GameManager gameManager = new GameManager();
+        assertThrows(StrategoGameRuleException.class, ()-> {
+            gameManager.checkConfig("tiny", returnTinyConfig());
+        });
+    }
+
 }
